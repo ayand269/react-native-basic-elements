42afa95c1104fcd20bf73e75bd7d980966fff212
ayand269 <ayand269@gmail.com>
2023-08-10 19:57:15 +0530

    Add New Functionality

---
+++ lib/Components/AppTextInput/AppTextInput.d.ts
@@ -0,0 +1,19 @@
+import React, { ReactElement } from 'react';
+import { TextStyle, ViewStyle, TextInputProps } from 'react-native';
+import { PropsOfIcon } from '../Icon';
+declare type Props = TextInputProps & {
+    inputStyle?: TextStyle | ViewStyle;
+    mainContainerStyle?: ViewStyle;
+    leftIcon?: PropsOfIcon;
+    rightAction?: ReactElement;
+    rightActionButtonStyle?: ViewStyle;
+    inputContainerStyle?: ViewStyle;
+    onRightIconPress?: () => void;
+    title?: string;
+    allowFontScaling?: boolean;
+    titleStyle?: TextStyle;
+    value?: string;
+    onChnageText?: (text: string) => void;
+};
+declare const AppTextInput: React.FC<Props>;
+export default AppTextInput;
---
+++ lib/Components/AppTextInput/index.d.ts
@@ -0,0 +1,32 @@
+/// <reference types="react" />
+declare const AppTextInput: import("react").FC<import("react-native").TextInputProps & {
+    inputStyle?: import("react-native").TextStyle | import("react-native").ViewStyle | undefined;
+    mainContainerStyle?: import("react-native").ViewStyle | undefined;
+    leftIcon?: import("../Icon").PropsOfIcon | undefined;
+    rightAction?: import("react").ReactElement<any, string | import("react").JSXElementConstructor<any>> | undefined;
+    rightActionButtonStyle?: import("react-native").ViewStyle | undefined;
+    inputContainerStyle?: import("react-native").ViewStyle | undefined;
+    onRightIconPress?: (() => void) | undefined;
+    title?: string | undefined;
+    allowFontScaling?: boolean | undefined;
+    titleStyle?: import("react-native").TextStyle | undefined;
+    value?: string | undefined;
+    onChnageText?: ((text: string) => void) | undefined;
+}> & {
+    Outlined: import("react").FC<import("react-native").TextInputProps & {
+        placeholder?: string | undefined;
+        borderWidth?: number | undefined;
+        containerStyle?: import("react-native").ViewStyle | undefined;
+        placeholderTextStyle?: import("react-native").TextStyle | undefined;
+        rightAction?: import("react").ReactElement<any, string | import("react").JSXElementConstructor<any>> | undefined;
+        rightActionButtonStyle?: import("react-native").ViewStyle | undefined;
+        onRightIconPress?: (() => void) | undefined;
+        value?: string | undefined;
+        onChangeText?: ((value: string) => void) | undefined;
+        inputStyle?: import("react-native").TextStyle | import("react-native").ViewStyle | undefined;
+        inputHeight?: number | undefined;
+        backgroundColor?: import("react-native").ColorValue | undefined;
+        placeholderTextSize?: number | undefined;
+    }>;
+};
+export default AppTextInput;
